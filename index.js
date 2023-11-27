e9ddbd2a137adb981095f0fd0910e980a451159e
Shahin992 <shahinkhan5979@gmail.com>
2023-11-28 03:28:06 +0600

    commit with user collection

--- index.js
+++ index.js
@@ -10,6 +10,54 @@ app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c60ctk1.mongodb.net/?retryWrites=true&w=majority`
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const UserCollection = client.db("SouthTechdb").collection("users");
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    app.post('/users', async(req,res) => {
+        const user = req.body;
+        const result = await UserCollection.insertOne(user);
+          res.send(result);
+        })
+        app.get("/users", async (req, res) => {
+            const result = await UserCollection.find().toArray();
+            res.send(result);})
+
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
 app.get("/", (req, res) => {
     res.send("SouthTech Server is Running");
   });
