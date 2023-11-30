eb4ba6b9719ed6443d8b7505f0f4b8eb19371f96
Shahin992 <shahinkhan5979@gmail.com>
2023-11-30 23:43:13 +0600

    commit with delete

--- index.js
+++ index.js
@@ -96,8 +96,14 @@ async function run() {
               console.log(req.body);
             });
 
-
-           
+ app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await UserCollection.deleteOne(query);
+      res.send(result);
+    });
+          
+    
 
 
          
